fa65e2a5087c436f211bf80a4dba88685301a03c
polyanskiy <n.polyanskiy@ase-ec.ru>
2024-06-26 17:51:25 +0300

    ConFix

--- DbModels/railwayContext.cs
+++ DbModels/railwayContext.cs
@@ -29,7 +29,8 @@ namespace NutriDbService.DbModels
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseNpgsql("Host=postgres.railway.internal;Port=38794;Username=postgres;Password=wTLZPRhYXHSReMKcUHSCNDEQlgQmbFDO;Database=railway");
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseNpgsql("Host=viaduct.proxy.rlwy.net;Port=38794;Username=postgres;Password=wTLZPRhYXHSReMKcUHSCNDEQlgQmbFDO;Database=railway");
             }
         }
 
