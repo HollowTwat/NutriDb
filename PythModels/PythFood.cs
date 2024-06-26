0ff6f3880ab96f360a1cbcba4b2ee16177293f2a
polyanskiy <n.polyanskiy@ase-ec.ru>
2024-06-27 00:30:02 +0300

    fix

--- PythModels/PythFood.cs
+++ PythModels/PythFood.cs
@@ -2,11 +2,10 @@
 {
     public class PythFood
     {
-
+        public string description { get; set; }
         public decimal weight { get; set; }
 
         public NutriProps nutritional_value { get; set; }
 
-        public string description { get; set; }
     }
 }
