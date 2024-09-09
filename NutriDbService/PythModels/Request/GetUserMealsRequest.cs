d23cd39e4dafbdb1f76461fb8755bf4e3264c0f8
polyanskiy <n.polyanskiy@rosatom.ru>
2024-09-10 02:49:32 +0300

    fix GetUserMeals

--- NutriDbService/PythModels/Request/GetUserMealsRequest.cs
+++ NutriDbService/PythModels/Request/GetUserMealsRequest.cs
@@ -1,11 +1,15 @@
-﻿namespace NutriDbService.PythModels.Request
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NutriDbService.PythModels.Request
 {
     public class GetUserMealsRequest
     {
+        [Required]
         public long userTgId { get; set; }
         public int? day { get; set; }
         public string dayStr { get; set; }
         public mealtype? typemeal { get; set; }
+        [Required]
         public Periods period { get; set; }
     }
 }
