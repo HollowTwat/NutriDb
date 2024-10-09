aafc76e09f9d0176c0eb69a10960665ad600e143
polyanskiy <n.polyanskiy@rosatom.ru>
2024-10-09 16:04:23 +0300

    fix helper

--- NutriDbService/Helpers/MealHelper.cs
+++ NutriDbService/Helpers/MealHelper.cs
@@ -50,6 +50,8 @@ namespace NutriDbService.Helpers
             Meal meal;
             if (createMealRequest.mealId == null)
             {
+                var IsTyme = DateTime.TryParseExact(createMealRequest.EatedAt, "dd.MM.yyyy_HH:mm", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out var parseTime);
+
                 meal = new Meal()
                 {
                     UserId = user.Id,
@@ -57,7 +59,8 @@ namespace NutriDbService.Helpers
                     Dishes = dishes,
                     Description = createMealRequest.meal.description,
                     Type = (short)createMealRequest.meal.type,
-                    MealTime = DateTime.UtcNow.ToLocalTime().AddHours(3)//DateTime.TryParseExact(createMealRequest.EatedAt, "dd.MM.yyyy_HH:mm", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out var parseTime) == true ? parseTime : null
+
+                    MealTime = IsTyme ? parseTime : DateTime.UtcNow.ToLocalTime().AddHours(3)
                 };
 
                 var oldmeal = _nutriDbContext.Meals.SingleOrDefault(x => x.UserId == meal.UserId && x.MealTime.Date == meal.MealTime.Date && x.Type == meal.Type && x.Type != 5);
--- NutriDbTest/UnitTest1.cs
+++ NutriDbTest/UnitTest1.cs
@@ -1,4 +1,8 @@
+using Newtonsoft.Json;
+using NutriDbService.DbModels;
 using NutriDbService.Helpers;
+using NutriDbService.PythModels.Request;
+using System.Text;
 
 namespace NutriDbTest
 {
@@ -15,6 +19,50 @@ namespace NutriDbTest
             long tgId = 389054202;
             PlotHelper plotHelper = new PlotHelper();
             plotHelper.SendPlot(values, labels, tgId, 2595.00M);
+            Assert.True(true);
+        }
+        [Fact]
+        public async void Test2()
+        {
+         
+            EditMealRequest request = new EditMealRequest()
+            {
+                userTgId = 186556585,
+                EatedAt = DateTime.Now.ToString("dd.MM.yyyy_HH:mm"),
+                meal = new NutriDbService.PythModels.PythMeal
+                {
+                    description = "TestFood",
+                    totalWeight = 350,
+                    type = NutriDbService.PythModels.mealtype.breakfast,
+                    food = new List<NutriDbService.PythModels.PythFood>
+                         {
+                            new NutriDbService.PythModels.PythFood
+                            {
+                                 description="Foodp1",
+                                weight=200,
+                                nutritional_value= new NutriDbService.PythModels.NutriProps
+                                 {
+                                     fats=30, carbs= 60, protein= 5, kcal=530
+                                 }
+                            },
+                             new NutriDbService.PythModels.PythFood
+                            {
+                                 description="Foodp2",
+                                weight=150,
+                                nutritional_value= new NutriDbService.PythModels.NutriProps
+                                 {
+                                     fats=1, carbs= 1, protein= 15, kcal=73
+                                 }
+                            }
+                         }
+                }
+            };
+            var reqUrl = "https://nutridb-production.up.railway.app/api/TypesCRUD/CreateMeal";
+            HttpClient client = new HttpClient();
+            client.Timeout = TimeSpan.FromSeconds(90);
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(reqUrl, content);
+            var r= await response.Content.ReadAsStringAsync();
             Assert.True(true);
         }
     }
