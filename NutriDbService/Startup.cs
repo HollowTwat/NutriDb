1fd333e15fbf592517e95fe3732e186f1715d48b
polyanskiy <n.polyanskiy@rosatom.ru>
2024-11-01 01:59:52 +0300

    test

--- NutriDbService/Startup.cs
+++ NutriDbService/Startup.cs
@@ -22,7 +22,7 @@ namespace NutriDbService
         {
             _configuration = configuration;
         }
-        // This method gets called by the runtime. Use this method to add services to the container.
+       
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors();
