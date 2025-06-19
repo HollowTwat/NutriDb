f31d876cde24741dbacf38c208ef340308d3414e
polyanskiy <n.polyanskiy@rosatom.ru>
2025-06-19 15:25:08 +0300

    fix

--- NutriDbService/Controllers/TypesCRUDController.cs
+++ NutriDbService/Controllers/TypesCRUDController.cs
@@ -1067,7 +1067,7 @@ namespace NutriDbService.Controllers
         }
 
         [HttpGet]
-        public async Task<bool> SendManualVoteNotify(long userTgId)
+        public async Task<bool> StartVoteNotify()
         {
 
             _ = Task.Run(async () =>
